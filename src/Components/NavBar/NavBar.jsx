cc35af6b1507ed8bcba8e06723ddf97b3673618a
erfolen <dlababy@mail.ru>
2021-01-14 18:55:07 +0300

    add NavBar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import style from "./NavBar.module.css";
+import {NavLink} from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <div>
-            Navbar
+        <div className={style.navbar}>
+            <div className={style.name}>
+                <h3>Каталог</h3>
+            </div>
+            <nav className={style.nav}>
+                <div className={style.item}>
+                    <NavLink exact to="/trotuarnai-plitka" activeClassName={style.active}>Тротуарная плитка</NavLink>
+                </div>
+                <div className={style.item}>
+                    <NavLink exact to="/vodostoki-gelob" activeClassName={style.active}>Водостоки, Желоб для отвода воды</NavLink>
+                </div>
+                <div className={style.item}>
+                    <NavLink exact to="/krishki" activeClassName={style.active}>Крышки на забор, на столбы</NavLink>
+                </div>
+                <div className={style.item}>
+                    <NavLink exact to="/bordurnii-kamen" activeClassName={style.active}>Бордюрный камень</NavLink>
+                </div>
+                <div className={style.item}>
+                    <NavLink exact to="/zabor" activeClassName={style.active}>Забор декоративный</NavLink>
+                </div>
+            </nav>
         </div>
     )
 }
